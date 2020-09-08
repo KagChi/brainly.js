dbcde491dd8a9158e9b1312e267df4406de875fe
KagChi <59391215+KagChi@users.noreply.github.com>
2020-09-08 18:30:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const axios = require("axios")
-const { version } = require("./package.json")
+const { author, version } = require("./package.json")
 
 const baseUrl = "https://brainly.hanifdwyputra.xyz"
 
 async brainlyData(query) {
-const getData = axios.get(baseUrl + '?q=' + query).then(res => res)
-return getData
+  const getData = axios.get(baseUrl + '?q=' + query).then(res => res)
+  return getData;
 }
+
+module.exports = { version, author, brainlyData}
