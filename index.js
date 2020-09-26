9f1fa9265ba2b5abf1a4b4c5a18c244e342f027c
カグチ <59391215+KagChi@users.noreply.github.com>
2020-09-26 22:24:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,13 @@ const axios = require("axios")
 const { author, version } = require("./package.json")
 
 const baseUrl = "https://brainly.hanifdwyputra.xyz"
-
-async function search(query){
-const data = axios.get(baseUrl + '?q=' + query).then(x => x.data.data.questionSearch.edges)
-let jawabanContent;
-for (let i=0; i < data.length; i++) {
-       jawabanContent += `${[i+1]} - Soal : ${answer[i].node.content}\nJawaban:\n${answer[i].node.answers.nodes[0].content}\n\n` 
-      }
-  return jawabanContent;
+const baseUrlv2 = "https://brainlya.hanifdwyputra.xyz"
+async function search(query) {
+const data = await axios.get(baseUrl + ?q= + query)
+return data;
 }
-
-async function brainlyData(query){
- console.log("brainlyData() will removed ad 1.2.0, use get() instead")
-const data = axios.get(baseUrl + '?q=' + query).then(x => x)
-  return data;
+async function searchV2(query) {
+const data = await axios.get(baseUrlv2 + ?q= + query)
+return data;
 }
-
-async function get(query){
-const data = axios.get(baseUrl + '?q=' + query).then(x => x.data)
-  return data;
-}
-
-module.exports = { version, author, search, brainlyData, get}
+module.exports = { version, author, search, searchV2 }
